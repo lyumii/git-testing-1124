43e69daefd8709d6e6e72acce18732930510ef98
lyumii <dmly1124@gmail.com>
2024-11-18 12:35:15 +0100

    new

---
+++ js.js
@@ -0,0 +1,65 @@
+console.log("velkommen til en introduksjon til javascript")
+
+// Variables
+
+// let number = 5
+// console.log(number)
+
+// const y = 10
+// console.log(y)
+
+// in js we use camelCase - every word after the first one is capitalized, no space
+
+// const introHeaderOne = document.getElementById("")
+
+// Datatypes
+
+//String
+
+let string = "text"
+
+//Integer
+
+let number = 10
+
+// Float 
+
+let float = 10.5
+
+// Boolean
+let boolean = true
+
+//Array`
+const fruits = ["Apple", "Orange", "Pear", "Banana"]
+console.log(fruits)
+
+const numbers = ["18", 15, 11.7]
+
+//Object
+const person = {
+    firstName: "Lyub",
+    lastName: "Dim",
+    age: 35,
+    hobbies: ["painting", "gym", "dogs", "gaming"]
+}
+console.log(person)
+
+//Operators
+
+let x = 6
+let y = 20
+let z = x + y
+console.log(z)
+
+let human = "lyumii"
+console.log(human)
+
+//Comparison operators
+if (x === y) {
+    console.log("x is equal to y");
+} else {
+     console.log("x is not equal to y");
+}
+
+
+
---
+++ js1.js
@@ -0,0 +1,24 @@
+const person = {
+    firstName: "Lyubka",
+    lastName: "Dimitrova",
+    age: 35,
+    hobbies: ["painting", "gym", "dogs", "gaming"],
+    livesIn: "Fitjar"
+}
+
+console.log(person);
+
+console.log(person.firstName);
+
+console.log(person.lastName);
+
+console.log(person.age);
+
+console.log(person.hobbies);
+
+console.log(person.hobbies[0]);
+console.log(person.hobbies[1]);
+console.log(person.hobbies[2]);
+console.log(person.hobbies[3]);
+
+console.log(person.livesIn);
